94c57baf878642f30f70f19a4e0f102047b29a9c
Kol4mo <47543776+kol4mo@users.noreply.github.com>
2024-02-23 13:54:53 -0700

    working movement anim

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using Unity.Mathematics;
 
 public class PlayerMovement : MonoBehaviour {
 	[SerializeField] Rigidbody2D rb;
+	[SerializeField] Animator animator;
 	[SerializeField] float speed = 5f;
 
 	private Vector2 movement;
@@ -14,6 +15,9 @@ public class PlayerMovement : MonoBehaviour {
 	void Update() {
 		movement.x = Input.GetAxisRaw("Horizontal");
 		movement.y = Input.GetAxisRaw("Vertical");
+		animator.SetFloat("Horizontal", movement.x);
+		animator.SetFloat("Vertical", movement.y);
+
 		//Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
 		/*if (Input.GetAxisRaw("Horizontal") > 0) {
 			rb.SetRotation(Vector3.Angle(Vector3.up, move) * -1);
