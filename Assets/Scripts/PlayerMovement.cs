0eab429a650cf62093cc72399024c79367dfdbef
Kol4mo <47543776+kol4mo@users.noreply.github.com>
2024-02-23 15:46:53 -0700

    added rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour {
 	[SerializeField] Rigidbody2D rb;
 	[SerializeField] Animator animator;
 	[SerializeField] float speed = 5f;
+	[SerializeField] GameObject target;
 
 	private Vector2 movement;
 	private void Start() {
@@ -17,6 +18,7 @@ public class PlayerMovement : MonoBehaviour {
 		movement.y = Input.GetAxisRaw("Vertical");
 		animator.SetFloat("Horizontal", movement.x);
 		animator.SetFloat("Vertical", movement.y);
+		animator.SetFloat("Speed", movement.sqrMagnitude);
 
 		//Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
 		/*if (Input.GetAxisRaw("Horizontal") > 0) {
@@ -24,6 +26,11 @@ public class PlayerMovement : MonoBehaviour {
 		} else {
 			rb.SetRotation(Vector3.Angle(Vector3.up, move));
 		}*/
+		if (movement.magnitude > 0) {
+			target.transform.position = gameObject.transform.position + new Vector3(movement.normalized.x, movement.normalized.y, 0);
+		} else {
+			target.transform.position = gameObject.transform.position + new Vector3(0, -1, 0);
+		}
 	}
 
 	private void FixedUpdate() {
