7755790750fb021de1d6d0cb2273c50b4912e077
Makenzie Buchenroth <mbuchenroth@student.neumont.edu>
2024-02-23 16:30:28 -0700

    .

--- Assets/Scripts/PickupBox.cs
+++ Assets/Scripts/PickupBox.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PickupBox : MonoBehaviour {
 	bool isCarried = false;
 	[SerializeField] GameObject target;
+	[SerializeField] Rigidbody2D rb;
 	GameObject box;
 
 	private void OnCollisionStay2D(Collision2D collision) {
@@ -28,11 +29,22 @@ public class PickupBox : MonoBehaviour {
 	private void Update() {
 		if (isCarried) {
 			box.transform.position = target.transform.position;
+			Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+			if (Input.GetAxisRaw("Horizontal") > 0) {
+
+				//rb.SetRotation(Vector3.Angle(Vector3.up, move) * -1);
+			}
+			else {
+				//rb.SetRotation(Vector3.Angle(Vector3.up, move));
+			}
 		}
 		if (Input.GetKey(KeyCode.F) && isCarried == true) {
 			Vector3 newPosition = box.transform.position;
-			box.transform.position = newPosition;
 			isCarried = false;
+			//box.transform.position = newPosition;
+			Vector3 temp = box.transform.position;
+			temp.x = Mathf.Round(temp.x);
+			box.transform.position = temp;
 		}
 	}
 }
