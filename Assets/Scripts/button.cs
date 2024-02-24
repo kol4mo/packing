99d592b5f65e2e6fe1f2fa28fc71eb8017aed29a
Makenzie Buchenroth <mbuchenroth@student.neumont.edu>
2024-02-24 12:07:59 -0700

    Added sounds

--- Assets/Scripts/button.cs
+++ Assets/Scripts/button.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class button : MonoBehaviour
-{
+[RequireComponent(typeof(AudioSource))]
+public class button : MonoBehaviour {
+	[SerializeField] private AudioSource audioSource;
+	[SerializeField] private AudioClip engine;
+
 	[SerializeField] VoidEvent buttonpress;
 	private void OnTriggerStay2D(Collider2D collision) {
 		if (collision.tag == "pickup") {
 			if (Input.GetKeyDown(KeyCode.E)) {
 				buttonpress.RaiseEvent();
+				audioSource.Play();
 			}
 		}
 	}
